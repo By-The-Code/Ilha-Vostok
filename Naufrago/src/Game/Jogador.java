d54dd92bdcd5dac56322cc606b2762e8dd65d771
Vinicius Araujo <vinicius.a.silveira99@gmail.com>
2023-03-11 20:28:07 -0300

    Mini ajustes na classe do jogador

--- Naufrago/src/Game/Jogador.java
+++ Naufrago/src/Game/Jogador.java
@@ -4,67 +4,62 @@ import java.util.Scanner;
 
 public class Jogador {
 	
-	private int vidaMaxima = 100;
-	private float vida = vidaMaxima;
-	private int forca = 0;
-	private int resistencia = 0;
-	private int nivel = 1;
-	private float xp = 0.0f;
-	private float xpParaUpar = 10.0f;
-	private String nome = "";
-	private String profissao = "";
-	private String arma = "Mãos";
-	private String armadura = "";
-	
-	
-	private void atribuindoValoresBaseadoNaProfissao(String profissao) {
-		
-		switch (profissao) {
-		
-		case "Médico":  		
-			setVidaMaxima(15);
-			setResistencia(5);
-			getStatus();
-			break;
-			
-			
-		case "Soldado":
-			setVidaMaxima(15);
-			setForca(5);
-			getStatus();
-			break;
-			
-		case "Professor":
-			setVidaMaxima(10);
-			setForca(3);
-			setResistencia(3);
-			getStatus();
-			break;	
-			
-		default: System.out.println("Comando Inválido!");
-		atribuindoValoresBaseadoNaProfissao("Professor");
-		}		
-		
-	}
+	private int 	vidaMaxima = 100;
+	private float 	vida = vidaMaxima;
+	private int 	forca = 0;
+	private int 	resistencia = 0;
+	private int 	nivel = 1;
+	private float 	xp = 0.0f;
+	private float 	xpParaUpar = 10.0f;
+	private String 	nome = "";
+	private String 	profissao = "";
+	private String 	arma = "Mãos";
+	private String 	armadura = "";
 	
 	public Jogador(String nome, int profissao) {
 		
 		this.nome = nome;
 		
 		switch(profissao) {
-		
-		case 1: this.profissao = "Médico";
-		atribuindoValoresBaseadoNaProfissao(this.profissao);
-		break;
-		
-		case 2: this.profissao = "Soldado";
-		atribuindoValoresBaseadoNaProfissao(this.profissao);
-		break;
-		
-		case 3: this.profissao = "Professor";
-		atribuindoValoresBaseadoNaProfissao(this.profissao);
-		break;
+			case 1: this.profissao = "Médico";
+			atribuindoValoresBaseadoNaProfissao(this.profissao);
+			break;
+			
+			case 2: this.profissao = "Soldado";
+			atribuindoValoresBaseadoNaProfissao(this.profissao);
+			break;
+			
+			case 3: this.profissao = "Professor";
+			atribuindoValoresBaseadoNaProfissao(this.profissao);
+			break;
 		}
+	}
+	
+	private void atribuindoValoresBaseadoNaProfissao(String profissao) {
+		
+		switch (profissao) {
+			case "Médico":  		
+				setVidaMaxima(15);
+				setResistencia(5);
+				getStatus();
+				break;
+				
+			case "Soldado":
+				setVidaMaxima(15);
+				setForca(5);
+				getStatus();
+				break;
+				
+			case "Professor":
+				setVidaMaxima(10);
+				setForca(3);
+				setResistencia(3);
+				getStatus();
+				break;	
+				
+			default: System.out.println("Comando Inválido!");
+			atribuindoValoresBaseadoNaProfissao("Professor");
+		}		
 	}
 
 	public void getStatus() {
@@ -79,29 +74,22 @@ public class Jogador {
 						+ 	"Arma: " 		+ arma			+ "\n"
 						+ 	"Armadura: " 	+ armadura 		+ "\n"
 						+ 	"XP p/ Upar: "  + xpParaUpar   );
-
 	}
 	
 	
-	public void passarDeNivel(float experiencia) {
+	public void passaDeNivel(float experiencia) {
 		
 		if(xp + experiencia >= xpParaUpar) {
-			
 			setXp(xp + experiencia - xpParaUpar);			
-			
 			setXpParaUpar(1.25f);
-			
 			setNivel(1);
-			
-			melhorarStatus();
+			melhoraStatus();
 		}
-		
 		else 
-			xp += experiencia;
-				
+			xp += experiencia;		
 	}
 	
-	public void melhorarStatus() {
+	public void melhoraStatus() {
 		
 		System.out.println("Escolha o que você quer melhorar! \n"
 						+  "1. Vida \n"
@@ -112,162 +100,128 @@ public class Jogador {
 		int escolha = scan.nextInt();
 		
 		switch (escolha) {
-		
-		case 1: 
-			setVidaMaxima(10);
-			setVida(vidaMaxima);
-			getStatus();
-			break;
-		case 2: 
-			setForca(1);
-			getStatus();
-			break;
-		case 3: 
-			setResistencia(1);
-			getStatus();
-			break;
-
-		default:
-			System.out.println("Comando inválido!");
-			
-		melhorarStatus();
-	}
-		
-}
+			case 1: 
+				setVidaMaxima(10);
+				setVida(vidaMaxima);
+				getStatus();
+				break;
+			case 2: 
+				setForca(1);
+				getStatus();
+				break;
+			case 3: 
+				setResistencia(1);
+				getStatus();
+				break;
 	
-	public int danoArma(String arma) {
-		
-		int var = 0;
+			default:
+				System.out.println("Comando inválido!");
+				melhoraStatus();
+		}	
+	}
+	
+	public int getDanoArma(String arma) {
 		
 		switch(arma) {
-		
-		case "Mãos":
-			var = 5;
-			return var;
-		
-		case "Braço do Assento":
-			var = 10;
-			return var;
-		
-		case "Pedaço de Madeira":
-			var = 10;
-			return var;
+			case "Mãos":
+				return 5;
 			
-		case "Osso Humano":
-			var = 15;
-			return var;
-		
-		case "Pinças de Formiga Gigante":
-			var = 30;
-			return var;
+			case "Braço do Assento":
+				return 10;
 			
-		default: System.out.println("Esta arma é inválida!");
-			return var;
+			case "Pedaço de Madeira":
+				return 10;
+				
+			case "Osso Humano":
+				return 15;
+			
+			case "Pinças de Formiga Gigante":
+				return 30;
+				
+			default: System.out.println("Esta arma é inválida!");
+					 return 0;
 		}
-		
 	}
 	
-	public float danoJogador() {
-		return getForca() + danoArma(arma);
+	public float getDanoJogador() {
 		// Falta definir a defesa do inimigo.
+		return getForca() + getDanoArma(arma);
 	}
 	
-	public int defesaArmadura(String armadura) {
-		
-		int var = 0;
+	public int getDefesaArmadura(String armadura) {
 		
 		switch(armadura) {
-		
-		case "Boné":
-			var = 5;
-			return var;
-		
-		case "A":
-			var = 10;
-			return var;
-		
-		case "B":
-			var = 10;
-			return var;
+			case "Boné":
+				return 5;
 			
-		case "C":
-			var = 15;
-			return var;
-		
-		case "D":
-			var = 30;
-			return var;
+			case "A":
+				return 10;
 			
-		default: System.out.println("Esta armadura é inválida!");
-			return var;
+			case "B":
+				return 10;
+				
+			case "C":
+				return 15;
+			
+			case "D":
+				return 30;
+				
+			default: System.out.println("Esta armadura é inválida!");
+				return 0;
 		}		
-		
 	}
 	
-	public float defesaJogador() {
-		return getResistencia() + defesaArmadura(armadura);
-		
+	public float getDefesaJogador() {
+		return getResistencia() + getDefesaArmadura(armadura);
 	}
 
 	public int getVidaMaxima() {
 		return vidaMaxima;
 	}
 
-
 	public void setVidaMaxima(int vidaMaxima) {
 		this.vidaMaxima += vidaMaxima;
 	}
-
 
 	public float getVida() {
 		return vida;
 	}
 
-
 	public void setVida(float vida) {
 		this.vida = vida;
 	}
-
 
 	public int getForca() {
 		return forca;
 	}
 
-
 	public void setForca(int forca) {
 		this.forca += forca;
 	}
-
 
 	public int getResistencia() {
 		return resistencia;
 	}
 
-
 	public void setResistencia(int resistencia) {
 		this.resistencia += resistencia;
 	}
-
 
 	public int getNivel() {
 		return nivel;
 	}
 
-
 	public void setNivel(int nivel) {
 		this.nivel += nivel;
 	}
-
 
 	public float getXp() {
 		return xp;
 	}
 
-
 	public void setXp(float xp) {
 		this.xp = xp;
 	}
-
 
 	public float getXpParaUpar() {
 		return xpParaUpar;
